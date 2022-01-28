e26a3ac5a8ed8f256daf20ebca072fb2515c816e
TudorTodea <96307705+TudorTodea@users.noreply.github.com>
2022-01-28 04:04:34 +0200

    Update ReportsController.php

--- phpLaravel-master/library_project/app/Http/Controllers/ReportsController.php
+++ phpLaravel-master/library_project/app/Http/Controllers/ReportsController.php
@@ -13,31 +13,7 @@ class ReportsController extends Controller
         return view('report.index');
     }
 
-    public function date_wise()
-    {
-        return view('report.dateWise', ['books' => '']);
-    }
-
-    public function generate_date_wise_report(Request $request)
-    {
-        $request->validate(['date' => "required|date"]);
-        return view('report.dateWise', [
-            'books' => book_issue::where('issue_date', $request->date)->latest()->get()
-        ]);
-    }
-
-    public function month_wise()
-    {
-        return view('report.monthWise', ['books' => '']);
-    }
-
-    public function generate_month_wise_report(Request $request)
-    {
-        $request->validate(['month' => "required|date"]);
-        return view('report.monthWise', [
-            'books' => book_issue::where('issue_date', 'LIKE', '%' . $request->month . '%')->latest()->get(),
-        ]);
-    }
+   
 
     public function not_returned()
     {
